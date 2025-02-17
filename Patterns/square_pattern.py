23ceeb21d03740684630ffa5fd275e90eeaa993b
GhulamFatima <fatimaghulam626@gmail.com>
2025-02-17 15:56:18 +0500

    patterns

---
+++ Patterns/square_pattern.py
@@ -0,0 +1,21 @@
+# ****
+# ****
+# ****
+# ****
+
+for i in range(0,4,1):
+    for j in range(0,4,1):
+        print("*", end="")
+    print()
+
+
+
+
+#in Javascript
+# for (let i =0;i<4;i++){
+#     for (let j=0;j<4;j++){
+#         process.stdout.write("*"); 
+#     }
+#     console.log()
+
+# }
