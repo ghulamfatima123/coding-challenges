606596ab14977a660090d3c6a479d48c6d824081
GhulamFatima <fatimaghulam626@gmail.com>
2025-02-17 16:14:41 +0500

    patterns

--- Patterns/square_pattern.py
+++ Patterns/square_pattern.py
@@ -3,13 +3,16 @@
 # ****
 # ****
 
-for i in range(0,4,1):
-    for j in range(0,4,1):
-        print("*", end="")
-    print()
+def square(n):
+    for i in range(0,n,1):
+        for j in range(0,n,1):
+            print("*", end="")
+        print()
 
-
-
+def main():
+    n=int(input("Enter the number of lines :"))
+    square(n)
+main()
 
 #in Javascript
 # for (let i =0;i<4;i++){
